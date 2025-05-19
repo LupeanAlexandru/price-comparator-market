5c233b49c93e47124b66d20255094bda72245827
Lupean Alexandru <lupeanalexandru99@gmail.com>
2025-05-19 16:43:30 +0300

    Added DTOs, CSV mapping and repositories

---
+++ src/main/java/com/example/price_comparator_market/dto/ProductDTO.java
@@ -0,0 +1,11 @@
+package com.example.price_comparator_market.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String category;
+    private Double brand;
+}
