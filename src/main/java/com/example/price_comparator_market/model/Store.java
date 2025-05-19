dff02e985838a6b5496210b18fd77010f51a1e8e
Lupean Alexandru <lupeanalexandru99@gmail.com>
2025-05-19 16:21:28 +0300

    Created models for products, stores and product prices

---
+++ src/main/java/com/example/price_comparator_market/model/Store.java
@@ -0,0 +1,20 @@
+package com.example.price_comparator_market.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "stores")
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
