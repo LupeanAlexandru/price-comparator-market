4998c6b61819b505caa84e6e79655e05d0662169
Lupean Alexandru <lupeanalexandru99@gmail.com>
2025-05-20 01:01:18 +0300

    Added database and sample data for testing

---
+++ src/main/java/com/example/price_comparator_market/exception/BusinessException.java
@@ -0,0 +1,17 @@
+package com.example.price_comparator_market.exception;
+
+import java.io.Serial;
+
+public class BusinessException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
